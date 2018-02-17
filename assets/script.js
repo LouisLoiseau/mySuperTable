07f9fc9931637485b164c6b9caa204f8903775d8
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2018-02-18 00:05:51 +0100

    gonne make the table again

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 function initSuperTable(attributes) {
     displayData(attributes['element'], attributes['data']);
-    console.log(attributes['options']['firstColumn']);
+    //rowHeight(attributes['options']['rowHeight']);
+    rowMouseOver(attributes['options']['rowMouseOver']);
+    rowStyle(attributes['options']['rowStyle']);
+    //fixedHeader(attributes['options']['fixedHeader']);
 }
 
-function displayData(element, data) {
+function displayData(element, data, firstColumn) {
     for (var i in data) {
         var id = '<div class="cell">' + data[i].id + '</div>';
         var index = '<div class="cell">' + data[i].index + '</div>';
@@ -18,5 +21,42 @@ function displayData(element, data) {
         var address = '<div class="cell">' + data[i].address + '</div>';
         var registered = '<div class="cell">' + data[i].registered + '</div>';
         element.innerHTML += '<div class="row">' + id + index + name + email + gender + isActive + age + eyeColor + company + phone + address + registered + '</div>';
+    }
+}
+
+function rowHeight(height) {
+    setTimeout(function () {
+        var allRows = document.querySelectorAll('.row');
+        for (var i in allRows) {
+            allRows[i].style.height = height + 'px';
+        }
+    }, 100);
+}
+
+function rowMouseOver(bool) {
+    if (typeof bool !== undefined) {
+        if (bool === true) {
+            var allRows = document.querySelectorAll('.row');
+            for (var i in allRows) {
+                allRows[i].className += ' hover-true';
+            }
+        }
+    }
+}
+
+function rowStyle(type) {
+    var allCells = document.querySelectorAll('.cell');
+    var allRows = document.querySelectorAll('.row');
+    if (type === 'line') {
+        for (var i in allCells) {
+            allCells[i].className += ' line';
+        }
+    } else if (type === 'zebra') {
+        for (var l in allRows) {
+            allRows[l].className += ' zebra';
+        }
+        for (var d in allCells) {
+            allCells[d].className += ' line';
+        }
     }
 }
