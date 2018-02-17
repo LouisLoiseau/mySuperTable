0052f6d22a58c12c80dde9c371ce03b52c155b82
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2018-02-17 20:19:15 +0100

    added data fct

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
-function displayData(element) {
-    for (var i in users) {
-        var id = '<div class="cell">' + users[i].id + '</div>';
-        var index = '<div class="cell">' + users[i].index + '</div>';
-        var name = '<div class="cell">' + users[i].name + '</div>';
-        var email = '<div class="cell">' + users[i].email + '</div>';
-        var gender = '<div class="cell">' + users[i].gender + '</div>';
-        var isActive = '<div class="cell">' + users[i].isActive + '</div>';
-        var age = '<div class="cell">' + users[i].age + '</div>';
-        var eyeColor = '<div class="cell">' + users[i].eyeColor + '</div>';
-        var company = '<div class="cell">' + users[i].company + '</div>';
-        var phone = '<div class="cell">' + users[i].phone + '</div>';
-        var address = '<div class="cell">' + users[i].address + '</div>';
-        var registered = '<div class="cell">' + users[i].registered + '</div>';
-        element.innerHTML += '<div class="row">' + id + index + name + email + gender + isActive + age + eyeColor + company + phone + address + registered + '</div>';
+function initSuperTable(attributes) {
+    for (var i in attributes) {
+        displayData(attributes['element'], attributes['data']);
     }
 }
 
-function initSuperTable(attributes) {
-    for (var i in attributes) {
-        displayData(attributes['element']);
+function displayData(element, data) {
+    for (var i in data) {
+        var id = '<div class="cell">' + data[i].id + '</div>';
+        var index = '<div class="cell">' + data[i].index + '</div>';
+        var name = '<div class="cell">' + data[i].name + '</div>';
+        var email = '<div class="cell">' + data[i].email + '</div>';
+        var gender = '<div class="cell">' + data[i].gender + '</div>';
+        var isActive = '<div class="cell">' + data[i].isActive + '</div>';
+        var age = '<div class="cell">' + data[i].age + '</div>';
+        var eyeColor = '<div class="cell">' + data[i].eyeColor + '</div>';
+        var company = '<div class="cell">' + data[i].company + '</div>';
+        var phone = '<div class="cell">' + data[i].phone + '</div>';
+        var address = '<div class="cell">' + data[i].address + '</div>';
+        var registered = '<div class="cell">' + data[i].registered + '</div>';
+        element.innerHTML += '<div class="row">' + id + index + name + email + gender + isActive + age + eyeColor + company + phone + address + registered + '</div>';
     }
 }
